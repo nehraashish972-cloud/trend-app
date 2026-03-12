76d2d0187ec9fcd1f502a8d31fa1ce45ba4be084
Asheesh Nehra <uname@DESKTOP-3T4MQ78.localdomain>
2026-03-12 16:29:29 +0530

    Initial Dockerized Trend App

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Trend Application Running in Docker on Port 3000");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
